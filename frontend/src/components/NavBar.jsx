06aeb5ce6daf2a8e0424eff55757acde81e012f4
Kamenchu <iankamenchu@gmail.com>
2025-01-16 22:38:42 +0300

    corrected syntax errors

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
 
     const navigate = useNavigate(); 
 
-    const [showMenu, setShowMenu] = useState(false)
+    // const [showMenu, setShowMenu] = useState(false)
     const [token, setToken] = useState(true)
 
   return (
